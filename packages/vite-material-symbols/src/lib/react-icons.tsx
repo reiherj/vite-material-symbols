774820ccff6dec583262196a21278b2b99120281
Janis Reiher <janis.reiher@alchemisten.ag>
2025-08-05 11:48:39 +0200

    chore: project setup

---
+++ packages/vite-material-symbols/src/lib/react-icons.tsx
@@ -0,0 +1,11 @@
+import styles from './react-icons.module.css';
+
+export function ReactIcons() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to ReactIcons!</h1>
+    </div>
+  );
+}
+
+export default ReactIcons;
