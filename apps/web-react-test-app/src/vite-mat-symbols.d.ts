23892a19ae9d91ae2f8feea09565cf8e691ec83a
Janis Reiher <janis.reiher@alchemisten.ag>
2025-08-12 16:08:59 +0200

    chore: set printWidth to 120

---
+++ apps/web-react-test-app/src/vite-mat-symbols.d.ts
@@ -0,0 +1,8 @@
+// src/vite-mat-symbols.d.ts
+declare module 'vite-mat-symbols' {
+  export const Home: string;
+  export const Menu: string;
+  export const Person: string;
+  const _default: string[];
+  export default _default;
+}
