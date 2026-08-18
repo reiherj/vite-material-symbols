1afd506a6072407366b7d084bf8e3559448e4e00
Janis Reiher <janis.reiher@alchemisten.ag>
2026-08-18 14:01:01 +0200

    chore: rename package and define exports

--- apps/web-react-test-app/vite.config.ts
+++ apps/web-react-test-app/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
-import { materialSymbolsPlugin } from '../../packages/vite-material-symbols/src';
+import { materialSymbolsPlugin } from '../../packages/vite-material-symbols/src/index';
 
 export default defineConfig(() => ({
   root: __dirname,
